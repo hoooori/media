78ac594fb6e63ce3c4dcbdf9ca9fc144c0c3ca8d
hoooori <hideyasu.horiuchi@gmail.com>
2019-03-11 20:29:30 +0900

    feat:set i18n, flash message, validation error message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  def create_flash_message(result, action, record, attribute)
+    I18n.t("flash.default.#{result}", action: I18n.t("actions.default.#{action}"), obj: "#{record.class.model_name.human}「#{record[attribute]}」")
+  end
 end
