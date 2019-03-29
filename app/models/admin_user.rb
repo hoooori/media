1d537dcb4ccd6dada957f55afb3c5c466448ecec
hoooori <hideyasu.horiuchi@gmail.com>
2019-03-29 23:58:54 +0900

    feature:made relation between admin_user and post

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class AdminUser < ApplicationRecord
-  # 使わないやつ→ :registerable, :confirmable, :lockable, :timeoutable, :omniauthable, :recoverable
+  # :registerable, :confirmable, :lockable, :timeoutable, :omniauthable, :recoverable
   # https://qiita.com/cigalecigales/items/73d7bd7ec59a001ccd74
   devise :database_authenticatable, :rememberable, :validatable, :trackable
+
+  has_many :posts, dependent: :delete_all
 end
