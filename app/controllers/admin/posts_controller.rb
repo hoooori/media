7fe705076c761089c9beef90cbd0be31a4307407
hoooori <hideyasu.horiuchi@gmail.com>
2019-03-31 22:58:41 +0900

    feature:implemented post pagination

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -7,7 +7,7 @@ class Admin::PostsController < ApplicationController
   before_action :authenticate_admin_user!
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc).page(params[:page]).per(6)
   end
 
   def new
